3b59108c1007633e765e7a3f8398b2caf2ddd69e
lizhenhua <lizhenhua230@gmail.com>
2015-11-12 12:24:30 +0800

    css3 animation

--- algorithm/test/moreHalf_test.js
+++
@@ -1,9 +0,0 @@
-var test = require('tape');
-var moreHalf = require('../moreHalf.js')
-
-test('the number which more than falf', (t) => {
-  t.equal(moreHalf.method1([1, 3, 2, 1, 1, 0, 1, 1]), 1);
-  t.equal(moreHalf.method1([3, 1, 2, 1, 1, 0, 1, 1]), 1);
-  t.equal(moreHalf.method1([1, 1]), 1);
-  t.end();
-});
